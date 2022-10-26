9cabf3746df2562b16807468faa64abe069d712c
ducanh4531 <ducanhle1794@gmail.com>
2022-10-27 06:09:09 +0800

    finish 10.20

--- src/components/SingleRepository.jsx
+++ src/components/SingleRepository.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-native";
-import { FlatList, View, Text } from "react-native";
+import { StyleSheet, FlatList, View, Text } from "react-native";
 import { format } from "date-fns";
 
 import RepositoryItem from "../components/RepositoryItem";
 import useRepository from "../hooks/useRepository";
 import { ItemSeparator } from "./RepositoryList";
+import theme from "../theme";
 
 const RepositoryInfo = ({ repository }) => {
 	return (
@@ -16,12 +17,30 @@ const RepositoryInfo = ({ repository }) => {
 };
 
 // Review details:
+const cardTitleStyles = StyleSheet.create({
+	container: {
+		flexDirection: "row",
+	},
+	reviewContainer: {
+		height: 35,
+		width: 35,
+		borderRadius: 35 / 2,
+		borderColor: theme.colors.primary,
+		borderWidth: 3,
+		flexDirection: "row",
+		justifyContent: "space-around",
+	},
+	reviewText: {
+		color: theme.colors.primary,
+		fontWeight: theme.fontWeights.bold,
+	},
+});
 
 const CardTitle = ({ review }) => {
 	return (
-		<View>
-			<View>
-				<Text>{review.rating}</Text>
+		<View style={cardTitleStyles.container}>
+			<View style={cardTitleStyles.reviewContainer}>
+				<Text style={cardTitleStyles.reviewText}>{review.rating}</Text>
 			</View>
 			<View>
 				<Text>{review.user.username}</Text>
@@ -38,9 +57,17 @@ const CardInformation = ({ reviewText }) => {
 		</View>
 	);
 };
+
+const reviewItemStyles = StyleSheet.create({
+	container: {
+		flexShrink: 1,
+		padding: 10,
+	},
+});
+
 const ReviewItem = ({ review }) => {
 	return (
-		<View>
+		<View style={reviewItemStyles.container}>
 			<CardTitle review={review} />
 			<CardInformation reviewText={review.text} />
 		</View>
